43cecdf261cffe6721c78eb73e3e5751f1fd33c6
Karthik Jagadeesha <kartu8@gmail.com>
2019-06-29 23:27:22 +0530

    Add user route to get user by id

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -2,9 +2,20 @@ const usersRouter = require('express').Router();
 const User = require('../models/User');
 
 usersRouter.get('/', async (_, response) => {
-  const users = await User.find({});
+  const users = await User.find({}, '-password');
   const parsedUsers = await users.map(user => user.toJSON());
   response.json(parsedUsers);
+});
+
+usersRouter.get('/:id', async (request, response) => {
+  const _id = request.params.id;
+  try {
+    const user = await User.findById(_id, '-password');
+    response.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    response.status(401).json({ error: 'This userId does not exist' });
+  }
 });
 
 module.exports = usersRouter;
