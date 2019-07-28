0a168956a8047c01019c40a8181047ed0a12830c
Karthik Jagadeesha <kartu8@gmail.com>
2019-07-28 11:21:19 +0530

    Add route for getting all relevant posts in feed

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -2,6 +2,7 @@ const feedRouter = require('express').Router();
 const ObjectId = require('mongoose').Types.ObjectId;
 
 const User = require('../models/User');
+const Post = require('../models/Post');
 
 feedRouter.get('/suggestions', async (request, response) => {
   const id = request.get('x-instaclone-userId');
@@ -25,6 +26,27 @@ feedRouter.get('/suggestions', async (request, response) => {
   } catch (error) {
     console.error(error);
     response.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+feedRouter.get('/', async (request, response) => {
+  const id = request.get('x-instaclone-userId');
+
+  try {
+    const { followingList } = await User.findById(id);
+    const data = await Post.aggregate([
+      {
+        $match: { 'owner.id': { $in: [id, ...followingList] } }
+      },
+      { $sort: { postedAt: -1 } },
+      { $addFields: { id: '$_id' } },
+      { $project: { _id: 0, __v: 0 } },
+      { $limit: 5 }
+    ]).exec();
+    response.status(200).json({ data });
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ error: 'Internal server code' });
   }
 });
 
