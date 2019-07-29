b5185264e48e0433d125277d1aafc2a60daa04ae
Karthik Jagadeesha <kartu8@gmail.com>
2019-07-29 18:43:45 +0530

    Add owner following information in userName endpoint

--- api/controllers/friend.js
+++ api/controllers/friend.js
@@ -3,10 +3,33 @@ const friendRouter = require('express').Router();
 const User = require('../models/User');
 
 friendRouter.get('/:userName', async (request, response) => {
+  const ownerId = request.get('x-instaclone-userId');
   const userName = request.params.userName;
 
   try {
-    const data = await User.findOne({ userName });
+    const result = await User.aggregate([
+      { $match: { userName } },
+      {
+        $addFields: {
+          ownerIsFollowing: { $in: [ownerId, '$followersList'] },
+          isFollowingOwner: { $in: [ownerId, '$followingList'] },
+          id: '$_id'
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          __v: 0,
+          followersList: 0,
+          followingList: 0,
+          phoneNumber: 0,
+          password: 0,
+          email: 0
+        }
+      }
+    ]).exec();
+
+    const data = result[0];
     if (data) {
       response
         .status(200)
